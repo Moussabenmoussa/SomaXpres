5a66a1b59643be0279ec2c9379c0639cc9770070
Moussabenmoussa <95001366+Moussabenmoussa@users.noreply.github.com>
2026-01-02 06:21:00 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# تهيئة قاعدة البيانات (Global)
 mongo = MongoClient(os.getenv("MONGO_URI"))
 db = mongo.get_database("somaxpres_db")
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = os.getenv("SECRET_KEY")
+    app.secret_key = os.getenv("SECRET_KEY", "dev_key_123")
 
-    # تسجيل المسارات (Blueprints)
+    # تسجيل المسارات
     from app.routes.dashboard import dashboard_bp
     from app.routes.public import public_bp
+    from app.routes.auth import auth_bp
     
     app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
-    app.register_blueprint(public_bp, url_prefix='/') # الروت الرئيسي
+    app.register_blueprint(public_bp, url_prefix='/')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
